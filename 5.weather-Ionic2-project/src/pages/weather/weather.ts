8d756ecc044d9af4c090e9f37761136446414877
EgorZima <zimaegor21@gmail.com>
2017-05-21 02:02:18 +0300

    Added 5th project. This weather search project.

---
+++ 5.weather-Ionic2-project/src/pages/weather/weather.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+import { WeatherService } from '../../app/services/weather.service'
+
+@Component({ 
+  selector: 'weather',
+  templateUrl: 'weather.html'
+})
+export class WeatherPage {
+
+  constructor(public navCtrl: NavController,
+              private weatherService: WeatherService) {}
+  
+  zmw;
+  weather;
+  search;
+  results;
+
+  
+  ngOnInit() {
+    this.getDefaultCity();
+    this.weatherService.getWeather(this.zmw).subscribe(
+      res => this.weather = res.current_observation
+    )
+
+  }
+
+  getQuery() {
+     this.weatherService.searchCities(this.search).subscribe(
+      res => { this.results = res.RESULTS
+          
+    }
+    )
+  }
+
+  chooseCity(city) {
+    this.results = [];
+
+    this.weatherService.getWeather(city.zmw).subscribe(
+      res => this.weather = res.current_observation
+    )
+
+  }
+  getDefaultCity() {
+    this.zmw = '00000.400.33562'
+  }
+        
+}
